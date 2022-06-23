1be855ddeca1de31b9ff4b587c4bc791651326e8
Pal <82200759+pal404error@users.noreply.github.com>
2022-06-23 14:55:12 +0530

    updated

--- insta.py
+++ insta.py
@@ -1,5 +1,5 @@
 from instabot import Bot
-import os 
+import os
 import glob
 cookie_del = glob.glob("config/*cookie.json")
 
@@ -7,35 +7,34 @@ os.system("rm -rf config")
 
 bot = Bot()
 
-bot.login(username = "#",
-		password = "#")
+bot.login(username = "#username here",
+		password = "#password here")
 bot.upload_photo("./img/quote1.jpg",
-				caption ="Enjoy, Inspire and motivate \n @citinghub\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
+				caption ="Enjoy, Inspire and motivate \n \n\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
 
 bot.upload_photo("./img/quote2.jpg",
-				caption ="Enjoy, Inspire and motivate \n @citinghub\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
+				caption ="Enjoy, Inspire and motivate \n \n\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
 
 bot.upload_photo("./img/quote3.jpg",
-				caption ="Enjoy, Inspire and motivate \n @citinghub\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
+				caption ="Enjoy, Inspire and motivate \n \n\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
 
 bot.upload_photo("./img/quote4.jpg",
-				caption ="Enjoy, Inspire and motivate \n @citinghub\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
+				caption ="Enjoy, Inspire and motivate \n \n\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
 
 bot.upload_photo("./img/quote5.jpg",
-				caption ="Enjoy, Inspire and motivate \n @citinghub\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
+				caption ="Enjoy, Inspire and motivate \n \n\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
 
 bot.upload_photo("./img/quote6.jpg",
-				caption ="Enjoy, Inspire and motivate \n @citinghub\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
+				caption ="Enjoy, Inspire and motivate \n \n\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
 
 bot.upload_photo("./img/quote7.jpg",
-				caption ="Enjoy, Inspire and motivate \n @citinghub\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
+				caption ="Enjoy, Inspire and motivate \n \n\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
 
 bot.upload_photo("./img/quote8.jpg",
-				caption ="Enjoy, Inspire and motivate \n @citinghub\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
+				caption ="Enjoy, Inspire and motivate \n \n\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
 
 bot.upload_photo("./img/quote9.jpg",
-				caption ="Enjoy, Inspire and motivate \n @citinghub\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
+				caption ="Enjoy, Inspire and motivate \n \n\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
 
 bot.upload_photo("./img/quote10.jpg",
-				caption ="Enjoy, Inspire and motivate \n @citinghub\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
-
+				caption ="Enjoy, Inspire and motivate \n \n\n \n #quotes are #quotation #quotess #quites #quotestagram #quoteslife #dailyquotes #quoted #quotestoliveby #quoteoftheday #quote")
